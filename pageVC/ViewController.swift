089e3658d3dfad9fddf98c3b593e46525882f2c2
XJL <xjl@xjldemacbook-pro.local>
2018-11-01 21:56:19 +0800

    Initial Commit

---
+++ pageVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pageVC
+//
+//  Created by XJL on 2018/11/1.
+//  Copyright © 2018年 XJL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
